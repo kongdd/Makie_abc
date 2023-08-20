4fcd0e94c868f8a53f9d447142b4afccecd94e46
Dongdong Kong <kongdd@users.noreply.github.com>
2023-08-20 23:51:19 +0800

    Fig2 volumeslice is not perfect

--- main_pkgs.jl
+++ main_pkgs.jl
@@ -5,6 +5,33 @@ using Printf
 using NaNStatistics
 
 
+function make_slider_3d(subfig, lon, lat, time)
+  sg = SliderGrid(subfig,
+    (label="lon", range=lon),
+    (label="lat", range=lat),
+    (label="time", range=time)
+  )
+
+  sl_yz, sl_xz, sl_xy = sg.sliders
+  set_close_to!(sl_yz, middle(lon))
+  set_close_to!(sl_xz, middle(lat))
+  set_close_to!(sl_xy, middle(time))
+
+  slon = sl_yz.value
+  slat = sl_xz.value
+  stime = sl_xy.value
+
+  i = @lift findall(lon .== $slon)[1]
+  j = @lift findall(lat .== $slat)[1]
+  k = @lift findall(time .== $stime)[1]
+  slon, slat, stime, i, j, k
+end
+
+## 另外一种方式
+
+
+
+
 function update_slice(plt, sl_yz::Slider, sl_xz::Slider, sl_xy::Slider)
   on(sl_yz.value) do v
     # 这里需要一个判断下标的操作
